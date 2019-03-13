9a0524385187f0858d353b2a111d0b329f3939d7
Brock Falfas <brockfalfas@gmail.com>
2019-03-13 19:01:07 -0400

    updated before standup. started the menu.js

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -8,3 +8,24 @@ const menu;
 // create a reference to the ".menu-button" class
 const menuButton;
 // Using your menuButton reference, add a click handler that calls toggleMenu
+
+class Articles {
+  constructor(articles) {
+    this.articles = articles;
+    this.articlesButtons = this.articles.querySelector('.articles-buttons');
+    this.articlesBtnOpen = this.articles.querySelector('.articles-btn-open');
+    this.articlesBtnClose = this.articles.querySelector('.articles-btn-close');
+    this.articlesContent = this.articles.querySelector('.articles-content');
+    this.articlesButtons.addEventListener('click', this.togglearticles.bind(this));
+    this.articlesButtons.addEventListener('click', () => this.togglearticles());
+  }
+  togglearticles() {
+    this.articlesBtnOpen.classList.toggle('hide-btn');
+    this.articlesBtnClose.classList.toggle('hide-btn');
+    this.articlesContent.classList.toggle('toggle-on');
+  }
+ }
+ const articless = document.querySelectorAll('.articles');
+ articless.forEach(function(articles){
+  return new articles(articles);
+ });
